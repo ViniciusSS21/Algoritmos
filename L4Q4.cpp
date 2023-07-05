20d0ba342b3ca7bdc9b3b73c790eca3382f590cd
ViniciusSS21 <138480087+ViniciusSS21@users.noreply.github.com>
2023-07-04 22:05:39 -0300

    L4Q4.cpp

--- L4Q4.cpp
+++ L4Q4.cpp
@@ -2,8 +2,10 @@
 #include <cstring>
 #include <cstdio>
 using namespace std;
+//aluno: Vinícius Santos da Silva
+//turma: IF23
+//4) programa que leia uma string e a imprima de trás para a frente:
 int main() {
-   //4) programa que leia uma string e a imprima de trás para a frente:
    string a;
    getline(cin, a);
    for (int i = a.size() - 1; i >= 0; --i) {

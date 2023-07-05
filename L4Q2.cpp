889c372b91ab22a0e5e5e2e25c9794a747b99370
ViniciusSS21 <138480087+ViniciusSS21@users.noreply.github.com>
2023-07-04 22:04:04 -0300

    L4Q2.cpp

--- L4Q2.cpp
+++ L4Q2.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 #include <cstring>
 using namespace std;
+//aluno: Vinícius Santos da Silva
+//turma: IF23
+//2)programa que leia uma string e imprima as quatro primeiras letras dela:
+
 int main() {
-  //2)programa que leia uma string e imprima as quatro primeiras letras dela:
   char a[100];
   cin.get(a,5);
   cout << a;

713891759fb268c44b7ddafb076e6b83678e9326
ViniciusSS21 <138480087+ViniciusSS21@users.noreply.github.com>
2023-07-04 22:19:56 -0300

    Add files via upload

---
+++ L4Q8.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstring>
+#include <string>
+#include <cstdlib>
+using namespace std;
+//aluno:Vinícius Santos da Silva
+//turma:IF23
+//8) Construa um programa que leia duas strings do teclado. Imprima uma mensagem informando se a segunda string lida está contida dentro da primeira.
+int main() {
+  
+string s1;
+getline(cin,s1);
+string s2;
+getline(cin,s2);
+auto s = s1.find(s2); // std::size_t
+if (s != string::npos){ // se não encontra, retorna string::npos
+cout << "encontrada "<< endl;
+}else
+cout << "não encontrada";
+  
+   
+}

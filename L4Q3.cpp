877743ccb19402f35bce0a435aff7c332de75e54
ViniciusSS21 <138480087+ViniciusSS21@users.noreply.github.com>
2023-07-04 22:04:54 -0300

    L4Q3.cpp

--- L4Q3.cpp
+++ L4Q3.cpp
@@ -2,8 +2,10 @@
 #include <cstring>
 #include <cstdio>
 using namespace std;
+//aluno: Vinícius Santos da Silva
+//turma: IF23
+//3)programa que leia e imprima quantos caracteres ela possui:
 int main() {
-  //3)programa que leia e imprima quantos caracteres ela possui:
   string a;
   getline(cin,a);
   cout << a.size();
